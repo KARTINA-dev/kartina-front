f2356aedb9983b6d8570d0ff75be5e950af4873c
Andrey Velts <andrey.veltts@nedra.digital>
2022-02-03 23:07:14 +0300

    feat(starter): Добавили стартер-кит для проекта

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb", "airbnb-typescript"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "no-shadow": "off",
+    "@typescript-eslint/no-shadow": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [1, { extensions: [".tsx", ".ts"] }],
+  },
+  ignorePatterns: ["vite.config.ts", "*.js"],
+};
