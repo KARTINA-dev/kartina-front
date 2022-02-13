99c1eff6d478d26072a12e6b0a86375dcf401685
Andrey Velts <andrey.veltts@nedra.digital>
2022-02-13 16:31:31 +0300

    feat(fonts): Добавили шрифты для проекта

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,15 @@
-import { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
 
-function App() {
-  const [count, setCount] = useState(0);
+import { history, store } from '../store';
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is:
-            {' '}
-            {count}
-          </button>
-        </p>
-        <p>
-          Edit
-          {' '}
-          <code>App.tsx</code>
-          {' '}
-          and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export const App: React.FC = () => (
+  <React.StrictMode>
+    <Provider store={store}>
+      <Router history={history}>
+        <Main />
+      </Router>
+    </Provider>
+  </React.StrictMode>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import checker from 'vite-plugin-checker';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(() => {
+  return {
+    publicDir: resolve(process.cwd(), 'public'),
+    root: 'src',
+    plugins: [
+      react(),
+      svgr(),
+      checker({
+        overlay: true,
+        eslint: {
+          extensions: ['.ts', '.tsx'],
+          files: ['./'],
+        },
+        typescript: {
+          tsconfigPath: resolve(process.cwd(), 'tsconfig.json'),
+        },
+      }),
+    ],
+    build: {
+      outDir: resolve(process.cwd(), 'dist'),
+    },
+  };
 });
