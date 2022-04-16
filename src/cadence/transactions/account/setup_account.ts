073d24fc9f0205efe446e3a896b26d39d06d29ab
spinaBelaya <serjd2112@gmail.com>
2022-04-16 17:12:25 +0300

    🦝 ref(setup)

--- src/cadence/transactions/account/setup_account.ts
+++ src/cadence/transactions/account/setup_account.ts
@@ -1,6 +1,7 @@
 export const SETUP_ACCOUNT = `
 import NonFungibleToken from 0xNonFungibleToken
 import KartinaItems from 0xKartinaItems
+import NFTStorefront from 0xStorefront
 
 // This transaction configures an account to hold Kartina Items.
 
@@ -17,6 +18,18 @@ transaction {
 
             // create a public capability for the collection
             signer.link<&KartinaItems.Collection{NonFungibleToken.CollectionPublic, KartinaItems.KartinaItemsCollectionPublic}>(KartinaItems.CollectionPublicPath, target: KartinaItems.CollectionStoragePath)
+        }
+        
+        if signer.borrow<&NFTStorefront.Storefront>(from: NFTStorefront.StorefrontStoragePath) == nil {
+
+            // Create a new empty .Storefront
+            let storefront <- NFTStorefront.createStorefront()
+
+            // save it to the account
+            signer.save(<-storefront, to: NFTStorefront.StorefrontStoragePath)
+
+            // create a public capability for the .Storefront
+            signer.link<&NFTStorefront.Storefront{NFTStorefront.StorefrontPublic}>(NFTStorefront.StorefrontPublicPath, target: NFTStorefront.StorefrontStoragePath)
         }
     }
 }
